eeadca3724455b4ca562d3568b172ff0302cfed2
zetahvdev <35743005+zetahvdev@users.noreply.github.com>
2021-05-31 14:43:33 -0600

    commit test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './sass/global.css';
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Don't learn React
         </a>
       </header>
     </div>
